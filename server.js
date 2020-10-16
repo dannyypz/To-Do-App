866f4154fd8cc5df8b453d0c2a75e026f73c7439
Daniel_Yepez <dannyypz@gmail.com>
2020-10-16 12:25:56 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -3,29 +3,111 @@
 /**
  * Required External Modules
  */
+
 const express = require("express");
 const path = require("path");
+
 /**
  * App Variables
  */
+
 const app = express();
 const port = process.env.PORT || "8000";
+
 /**
  *  App Configuration
  */
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 /**
  * Routes Definitions
  */
+
 app.get("/", (req, res) => {
-    res.status(200).send("TO DO App");
-  });
+  res.status(200).send("TO DO App");
+});
+
+// this route will return all of the existing To Do items
+
+const items = [];
+let id = 0;
+
+app.get("/api/items", (req, res, next) => {
+  res.json(items);
+});
+
+// this route will create a new to do item
+
+app.post("/api/items", (req, res, next) => {
+  if (req.body.item) {
+    id = id + 1;
+    const newItem = {
+      id: id,
+      item: req.body.item,
+      completed: false,
+    };
+    items.push(newItem);
+    res.json(newItem);
+  } else {
+    res.status(400).json({ error: "item needs a description" });
+  }
+});
+
 /**
  * Server Activation
  */
 
 app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-  });
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+// // server.js
+
+
+// /**
+//  * Required External Modules
+//  */
+// const express = require("express");
+// // const path = require("path");
+// /**
+//  * App Variables
+//  */
+// const app = express();
+// const port = process.env.PORT || 3000;
+// /**
+//  *  App Configuration
+//  */
+// app.use(express.json());
+// app.use(express.static( 'public'));
+
+// /**
+//  * Routes Definitions
+//  */
+// const items = [];
+// let id = 0;
+
+
+// // app.get("/", (req, res) => {
+// //     res.status(200).send("TO DO App");
+// //   });
+
+// app.get('/items', (req, res, next) => {
+//     res.json(items);
+// });
+
+
+// /**
+//  * Server Activation
+//  */
+
+// app.listen(port, () => {
+//     console.log(`Listening to requests on http://localhost:${port}`);
+//   });
